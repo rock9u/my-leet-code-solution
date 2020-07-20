bfdd1c48e66ff9cab639738d28a185ab352e25c9
Rock Gu <guf@mcmaster.ca>
2020-07-20 16:55:41 -0400

    group anagrams

---
+++ 49. Group Anagrams.py
@@ -0,0 +1,20 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        '''
+        for each word
+            count letter
+            put each item in map
+        for each keypair
+            print
+            
+        '''
+
+        bucket = dict()
+        for word in strs:
+            key = ''.join(sorted(word))
+            if not key in bucket.keys():   
+                bucket[key] = [word]
+            else:
+                bucket[key].append(word)
+
+        return bucket.values()
