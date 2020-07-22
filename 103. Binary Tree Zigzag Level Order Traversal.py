cb38826c69d50b287febbc99ac295bfeee19143e
Rock Gu <guf@mcmaster.ca>
2020-07-22 11:54:35 -0400

    LC binary tree zigzag, using list pop

---
+++ 103. Binary Tree Zigzag Level Order Traversal.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        self.result = []
+        self.levelOrder([root])
+        return self.result
+    
+    def levelOrder(self, thisRowStack,isToRight=True):
+        if len(thisRowStack) == 0 :
+            return
+        newRowStack = []
+        thisRowDisplay = []
+
+        # print("entering levelOrder")
+        while len(thisRowStack) > 0:
+            node = thisRowStack.pop()
+            if node:
+                thisRowDisplay.append(node.val)
+                if isToRight:
+                    newRowStack.append(node.left)
+                    newRowStack.append(node.right)
+                else:
+                    newRowStack.append(node.right)
+                    newRowStack.append(node.left)
+        if thisRowDisplay != []:
+            self.result.append(thisRowDisplay)
+        return self.levelOrder(newRowStack,not isToRight)
+
+
